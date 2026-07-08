0911a16a9abd345c1f34386ef0e7b1d06d599ec2
Santiago Gomez Marin <sgmarin@inforcol.local>
2026-07-08 08:41:15 -0500

    Productos

--- mi-app-angular/src/app/app.module.ts
+++ mi-app-angular/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductCarComponent } from './product-car/product-car.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductCardComponent,
+    ProductCarComponent,
+    ProductListComponent,
     
   ],
   imports: [
---
+++ mi-app-angular/src/app/product-car/product-car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-car',
+  templateUrl: './product-car.component.html',
+  styleUrls: ['./product-car.component.css']
+})
+export class ProductCarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
