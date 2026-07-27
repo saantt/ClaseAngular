a7078cc9b2795647e0deb70f01f45d72c1cd08ce
Santiago Gomez Marin <sgmarin@inforcol.local>
2026-07-27 08:57:28 -0500

    feat: se creo la libreria

--- mi-app-angular/src/app/directives/highlight-book.directive.ts
+++ mi-app-angular/src/app/directives/highlight-book.directive.ts
@@ -11,41 +11,25 @@ import {
 export class HighlightBookDirective {
 
   constructor(
-
     private element: ElementRef,
-
     private renderer: Renderer2
-
   ) { }
 
   @HostListener('mouseenter')
-
   mouseEnter() {
-
     this.renderer.setStyle(
-
       this.element.nativeElement,
-
       'background',
-
       '#fff8dc'
-
     );
-
   }
 
   @HostListener('mouseleave')
-
   mouseLeave() {
-
     this.renderer.removeStyle(
-
       this.element.nativeElement,
-
       'background'
-
     );
-
   }
 
 }
