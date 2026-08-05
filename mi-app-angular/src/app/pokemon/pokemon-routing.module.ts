cfb818bc63eb0cee77f63515386a8ee856834e74
Sant <santiagomaring05@gmail.com>
2026-08-05 10:53:24 -0500

    feat: se arreglaron los errores 2.0

--- mi-app-angular/src/app/pokemon/pokemon-routing.module.ts
+++ mi-app-angular/src/app/pokemon/pokemon-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: PokemonListComponent},
+  {path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard]},
   {path: ':name', component: PokemonDetailComponent}
+  
 ];
 
 @NgModule({
